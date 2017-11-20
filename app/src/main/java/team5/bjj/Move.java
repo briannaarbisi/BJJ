aad93f054f0f2b7362d8db1bf441c77c56ba5aa7
Cindy Wood <cindywood@Cindys-MacBook-Pro.local>
2017-11-19 18:23:20 -0600

    Adding Move class implementation

--- app/src/main/java/team5/bjj/Move.java
+++ app/src/main/java/team5/bjj/Move.java
@@ -1,8 +1,21 @@
 package team5.bjj;
 
 /**
- * Created by cindywood on 11/16/17.
+ * Created by Hannah on 11/16/17.
  */
 
 public class Move {
+    private String name;
+
+    public Move(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
